b848864f614ffb280991e1bf6f658f4572a4d83f
Anmol Krishhhhhhh <162597538+Anmol-G-K@users.noreply.github.com>
2025-03-18 20:32:38 +0530

    final code push

---
+++ src/Glove.ino
@@ -0,0 +1,168 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <MPU6050.h>
+
+// Wi-Fi and MQTT Credentials
+const char *ssid = "Your_SSID";
+const char *password = "Your_PASSWORD";
+const char *mqttServer = "broker.emqx.io";
+const int mqttPort = 1883;
+const char *mqttUser = "Your_MQTT_Username";
+const char *mqttPassword = "Your_MQTT_Password";
+
+// MQTT client
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+// MPU6050 Sensors
+MPU6050 mpuServo, mpuStepper;
+
+// FSR Configuration
+#define FSR1_PIN 34 // FSR for Gripper
+#define FSR2_PIN 35 // Second FSR for additional grip check
+int fsr1Value, fsr2Value;
+
+// Kalman Filter Variables
+float angleServoX, angleServoY, angleStepperX;
+float biasServoX = 0, biasServoY = 0, biasStepperX = 0;
+float P[2][2] = {{1, 0}, {0, 1}};
+const float kalmanQ = 0.001, kalmanR = 0.03, dt = 0.05; // Smooth update delay
+
+// Function Prototypes
+void connectToWiFi();
+void ensureWiFiConnection();
+void connectToMQTT();
+void ensureMQTTConnection();
+void kalmanFilter(float &angle, float &bias, float newAngle, float newRate);
+void sendAnglesToMQTT(float angleServoX, float angleServoY, float angleStepperX, bool gripActive);
+
+// Delay for smoother updates
+unsigned long previousMillis = 0;
+const long interval = 200; // Adjusted update interval for smooth transition
+
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin();
+
+    // Initialize MPU6050 sensors
+    mpuServo.initialize();
+    mpuStepper.initialize();
+
+    if (!mpuServo.testConnection() || !mpuStepper.testConnection())
+    {
+        Serial.println("MPU6050 initialization failed!");
+        while (1)
+            ;
+    }
+
+    mqttClient.setServer(mqttServer, mqttPort);
+    connectToWiFi();
+    connectToMQTT();
+}
+
+void loop()
+{
+    ensureWiFiConnection();
+    ensureMQTTConnection();
+
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+        previousMillis = currentMillis;
+
+        // Read MPU6050 data for Servos
+        int16_t ax, ay, az, gx, gy, gz;
+        mpuServo.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+        float accelAngleX = atan2(ay, sqrt(ax * ax + az * az)) * 180.0 / M_PI;
+        float accelAngleY = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / M_PI;
+        float gyroRateX = gx / 131.0, gyroRateY = gy / 131.0;
+        kalmanFilter(angleServoX, biasServoX, accelAngleX, gyroRateX);
+        kalmanFilter(angleServoY, biasServoY, accelAngleY, gyroRateY);
+
+        // Read MPU6050 data for Stepper
+        int16_t ax2, ay2, az2, gx2, gy2, gz2;
+        mpuStepper.getMotion6(&ax2, &ay2, &az2, &gx2, &gy2, &gz2);
+        float accelStepperX = atan2(ay2, sqrt(ax2 * ax2 + az2 * az2)) * 180.0 / M_PI;
+        float gyroStepperX = gx2 / 131.0;
+        kalmanFilter(angleStepperX, biasStepperX, accelStepperX, gyroStepperX);
+
+        // Read FSR sensor values
+        fsr1Value = analogRead(FSR1_PIN);
+        fsr2Value = analogRead(FSR2_PIN);
+        bool gripActive = (fsr1Value > 500 || fsr2Value > 500);
+
+        // Send data via MQTT
+        sendAnglesToMQTT(angleServoX, angleServoY, angleStepperX, gripActive);
+    }
+}
+
+void sendAnglesToMQTT(float angleServoX, float angleServoY, float angleStepperX, bool gripActive)
+{
+    char msg[50];
+    snprintf(msg, sizeof(msg), "SX:%.2f,SY:%.2f,ST:%.2f,GR:%d", angleServoX, angleServoY, angleStepperX, gripActive);
+    mqttClient.publish("robot/angles", msg);
+}
+
+void kalmanFilter(float &angle, float &bias, float newAngle, float newRate)
+{
+    float rate = newRate - bias;
+    angle += dt * rate;
+    P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + kalmanQ);
+    P[0][1] -= dt * P[1][1];
+    P[1][0] -= dt * P[1][1];
+    P[1][1] += kalmanQ * dt;
+    float S = P[0][0] + kalmanR;
+    float K[2] = {P[0][0] / S, P[1][0] / S};
+    float y = newAngle - angle;
+    angle += K[0] * y;
+    bias += K[1] * y;
+    P[0][0] -= K[0] * P[0][0];
+    P[0][1] -= K[0] * P[0][1];
+    P[1][0] -= K[1] * P[0][0];
+    P[1][1] -= K[1] * P[0][1];
+}
+void ensureMQTTConnection()
+{
+    if (!mqttClient.connected())
+    {
+        connectToMQTT();
+    }
+}
+void connectToMQTT()
+{
+    while (!mqttClient.connected())
+    {
+        Serial.print("Connecting to MQTT...");
+        if (mqttClient.connect("ESP32_DATA_ACQ", mqttUser, mqttPassword))
+        {
+            Serial.println("Connected to MQTT Broker.");
+        }
+        else
+        {
+            Serial.print("Failed. State=");
+            Serial.println(mqttClient.state());
+            delay(2000);
+        }
+    }
+}
+void ensureWiFiConnection()
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        connectToWiFi();
+    }
+}
+void connectToWiFi()
+{
+    Serial.print("Connecting to Wi-Fi: ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nWi-Fi Connected.");
+}
---
+++ src/Power_Drive.ino
@@ -0,0 +1,124 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <AccelStepper.h>
+
+// PWM Servo Driver
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+
+// Servo Channels
+// #define BASE_SERVO 0
+#define SHOULDER_SERVO 0
+#define ELBOW_SERVO 1
+#define WRIST_SERVO 2
+
+// Stepper Motor (Base Rotation)
+#define STEP_PIN 26
+#define DIR_PIN 27
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+
+// Servo Angles
+int baseAngle = 90, shoulderAngle = 90, elbowAngle = 90, wristAngle = 90;
+bool gripperClosed = false;
+
+// Wi-Fi and MQTT Credentials
+const char *ssid = "Your_SSID";
+const char *password = "Your_PASSWORD";
+const char *mqttServer = "broker.emqx.io";
+const int mqttPort = 1883;
+const char *mqttUser = "Your_MQTT_Username";
+const char *mqttPassword = "Your_MQTT_Password";
+
+// MQTT Client
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+// Function Prototypes
+void updateServos();
+void mqttCallback(char *topic, byte *message, unsigned int length);
+int angleToPulse(int angle);
+void controlStepper(float angle);
+
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin();
+
+    pwm.begin();
+    pwm.setPWMFreq(50);
+
+    stepper.setMaxSpeed(200);
+    stepper.setAcceleration(100);
+
+    WiFi.begin(ssid, password);
+    mqttClient.setServer(mqttServer, mqttPort);
+    mqttClient.setCallback(mqttCallback);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("WiFi connected!");
+
+    while (!mqttClient.connected())
+    {
+        Serial.print("Connecting to MQTT...");
+        if (mqttClient.connect("ESP32_ACTUATOR", mqttUser, mqttPassword))
+        {
+            Serial.println("Connected to MQTT.");
+            mqttClient.subscribe("robot/angles");
+        }
+        else
+        {
+            Serial.println("Failed. Retrying...");
+            delay(2000);
+        }
+    }
+    updateServos();
+}
+
+void loop()
+{
+    mqttClient.loop();
+    stepper.run();
+}
+
+void mqttCallback(char *topic, byte *message, unsigned int length)
+{
+    String data;
+    for (unsigned int i = 0; i < length; i++)
+    {
+        data += (char)message[i];
+    }
+
+    float angleServoX, angleServoY, angleStepperX;
+    int grip;
+    sscanf(data.c_str(), "SX:%f,SY:%f,ST:%f,GR:%d", &angleServoX, &angleServoY, &angleStepperX, &grip);
+
+    shoulderAngle = map(angleServoX, -90, 90, 0, 180);
+    elbowAngle = map(angleServoY, -90, 90, 0, 180);
+    gripperClosed = grip;
+
+    controlStepper(angleStepperX);
+    updateServos();
+}
+
+void controlStepper(float angle)
+{
+    stepper.moveTo(map(angle, -90, 90, -1000, 1000));
+}
+
+void updateServos()
+{
+    pwm.setPWM(SHOULDER_SERVO, 0, angleToPulse(shoulderAngle));
+    pwm.setPWM(ELBOW_SERVO, 0, angleToPulse(elbowAngle));
+    pwm.setPWM(WRIST_SERVO, 0, angleToPulse(gripperClosed ? 0 : 180));
+}
+
+int angleToPulse(int angle)
+{
+    int pulse = map(angle, 0, 180, 150, 600); // 150-600 is a good range for most servos
+    return pulse;
+}
